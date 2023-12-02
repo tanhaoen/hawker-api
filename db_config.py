199aa5dcdf8c92e5128b1d0640ef166db7876878
tanhaoen <nathanieltan1701@gmail.com>
2023-12-02 18:58:51 +0800

    Added DB prefix to environment variables

--- db_config.py
+++ db_config.py
@@ -6,4 +6,4 @@ load_dotenv()
 DRIVER = '{ODBC Driver 18 for SQL Server}'
 
 def get_pyodbc_connection_string():
-    return f'DRIVER={DRIVER};SERVER=tcp:{os.environ["SERVER"]};PORT=1433;DATABASE={os.environ["DATABASE"]};UID={os.environ["USERNAME"]};PWD={{{os.environ["PASSWORD"]}}};'
+    return f'DRIVER={DRIVER};SERVER=tcp:{os.environ["DB_SERVER"]};PORT=1433;DATABASE={os.environ["DATABASE"]};UID={os.environ["DB_USERNAME"]};PWD={{{os.environ["DB_PASSWORD"]}}};'
